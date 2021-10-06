f04fa5aa86f75f20bfff8b9d5c1b2f812b628757
Adam Patrick <coderap931@gmail.com>
2021-10-06 19:33:58 -0400

    + ability to perform multiple searches w/o refresh

--- public/main.js
+++ public/main.js
@@ -6,13 +6,16 @@ let searchButton = document.getElementById("searchButton");
 searchButton.addEventListener('click', searchGiphy);
 
 async function searchGiphy() {
+    let display = document.getElementById("container");
+    while(display.firstChild){
+        display.removeChild(display.firstChild)
+    }
     let searchTerms = document.getElementById("searchTerms");
     let userQuery = searchTerms.value;
     let response = await fetch(`${coreURL}?api_key=${apiKey}&q=${userQuery}&limit=${displayLimit}`);
     let json = await response.json();
     for(i=0; i< displayLimit; i++){
         let currentGif = json.data[i].images.fixed_height.url;
-        let display = document.getElementById("container");
         let gif = document.createElement('img');
         gif.src = currentGif;
         display.appendChild(gif);
